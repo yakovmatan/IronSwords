4c15fe7121bb260370b69532978260695f85f898
yakov matan <yakovmatan@gmail.com>
2025-05-21 12:01:26 +0300

    interface for attack tool

---
+++ IAttackTool.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+interface IAttackTool
+{
+    string Name { get; set; }
+
+    int AmountAmmunition { get; set; }
+
+    int fuel { get; set; }
+
+    List<string> target { get; set; }
+}

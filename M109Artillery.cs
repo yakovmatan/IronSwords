1c143fbda0edd5d11190919a1edb239f1213d81c
yakov matan <yakovmatan@gmail.com>
2025-05-21 16:43:10 +0300

    create M109 class

---
+++ M109Artillery.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+public class  M109Artillery : IAttackTool
+{
+    public string Name { get; } = "M109";
+
+    public int fuel { get; private set; } = 80;
+
+    public int AmountAmmunition { get; private set; } = 3;
+
+    public List<string> target { get; } = new List<string> { "open area" };
+
+
+    public M109Artillery()
+    {
+        IDF.AddAttackTool(this);
+    }
+
+    public void Refuel(int amount)
+    {
+        if (amount > 0)
+        {
+            this.fuel += amount;
+        }
+    }
+
+    public void attack()
+    {
+        this.AmountAmmunition--;
+    }
+
+
+}

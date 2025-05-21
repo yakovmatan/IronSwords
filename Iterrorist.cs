f0978aae4571a0c1944ff548658d885f228d3543
Shlomo <sh.z.wind@gmail.com>
2025-05-21 11:58:30 +0300

    create interface Iterrorist

---
+++ Iterrorist.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IronSwords
+{
+    internal interface Iterrorist
+    {
+        string name { get; set; }
+        int rank { get; set; }
+        bool status { get; set; }
+        List<string> weapone { get; set; }
+    }
+}

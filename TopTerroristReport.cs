aaaa7b4c56d921182a5f761378538d349b0d81c0
Shlomo <sh.z.wind@gmail.com>
2025-05-22 15:06:34 +0300

    create class TopTerroristReport

---
+++ TopTerroristReport.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class TopTerroristReport
+{
+    private Dictionary<string, int> DictOfTerrorists;
+
+    public void DictTerroristReport()
+    {
+        foreach (var i in Aman.ReadList())
+        {
+            if (DictOfTerrorists.ContainsKey(i.Terrorist.name))
+            {
+                DictOfTerrorists[i.Terrorist.name] += 1;
+            }
+            else
+            {
+                DictOfTerrorists[i.Terrorist.name] = 1;
+            }
+        }
+    } 
+    public void ShowTopTerroristReport()
+    {
+        int maxValue = DictOfTerrorists.Values.Max();
+        foreach(var kv in DictOfTerrorists)
+        {
+            if(kv.Value == maxValue)
+            {
+                Console.WriteLine(kv.Key);
+            }
+        }
+    }
+}

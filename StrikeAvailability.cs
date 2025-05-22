03fd860b23c06da514c44fc7547fe20fe4b80d3d
yakov matan <yakovmatan@gmail.com>
2025-05-22 15:08:29 +0300

    create strideAvailability class

---
+++ StrikeAvailability.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class StrikeAvailability
+{
+    public void ShowStrikeAvailabilty()
+    {
+        foreach (var atteckTool in IDF.ReadList())
+        {
+            Console.WriteLine($"Attack tool: {atteckTool.Name}");
+            Console.WriteLine($"Available strikes left: {atteckTool.AmountAmmunition}");
+            Console.WriteLine();
+
+        }
+            
+
+    }
+}

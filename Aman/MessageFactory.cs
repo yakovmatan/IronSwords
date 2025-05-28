6114fa242b8041bafb61bec8e7ce2a678bf6459a
yakov matan <yakovmatan@gmail.com>
2025-05-28 15:53:47 +0300

    MessageFactory

---
+++ Aman/MessageFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class MessageFactory
+{
+    private Random Rand = new Random();
+    public void CreateMessage()
+    {
+        for (int i = 0; i < Rand.Next(10,21); i++)
+        {
+            new Message();
+        }
+
+    }
+}

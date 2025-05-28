458ccff65f0831407df424ca7c4ec1c29207e35c
Shlomo <sh.z.wind@gmail.com>
2025-05-28 16:02:48 +0300

    creat class TerroristFactory

---
+++ Hamas/TerroristFactory.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class TerroristFactory
+{
+    private List<string> terrorists = new List<string>
+    {
+    "Abu Karim",
+    "Jamal Al-Zein",
+    "Khaled Nasir",
+    "Fadi Al-Masri",
+    "Tariq Haddad",
+    "Omar Bakr",
+    "Yusuf Mahdi",
+    "Samir Qassem",
+    "Nabil Rahman",
+    "Adnan Zarif",
+    "Rami Khatib",
+    "Bassam Jaber",
+    "Walid Hamdan",
+    "Sami Al-Najjar",
+    "Zayd Amari",
+    "Imad Khalil",
+    "Nasser Farouk",
+    "Majid Yasin",
+    "Ibrahim Salim",
+    "Ali Darwish"
+    };
+    Random rand = new Random();
+    public void CreateTerrorist()
+    {
+        for (int i = 0; i<11; i++)
+        {
+            int index = rand.Next(terrorists.Count);
+            string terrorist = terrorists[index];
+            terrorists.RemoveAt(index);
+            new Terrorist(terrorist);
+        }
+    }
+    
+}

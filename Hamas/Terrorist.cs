494ccfa73d83598d2948a6ef5bb08f40cf8b59bb
yakov matan <yakovmatan@gmail.com>
2025-05-28 17:28:37 +0300

    update progrem

--- Hamas/Terrorist.cs
+++ Hamas/Terrorist.cs
@@ -9,25 +9,25 @@ public class Terrorist : ITerrorist
     public bool IsLife { get; set; } = true;
     public List<string> Weapons { get; private set; } = new List<string>();
 
+    private static Random random = new Random();
+
 
     public Terrorist(string name)
     {
         this.Name = name;
-        this.Rank = updateRank();
+        updateRank();
         updateWeapone();
         Hamas.AddTerrorist(this);
     }
 
 
-    public int updateRank()
+    public void updateRank()
     {
-        Random random = new Random();
         int therank = random.Next(1, 6);
-        return therank;
+        this.Rank = therank;
     }
     public void updateWeapone()
     {
-        Random random = new Random();
         int sum = random.Next(1, 4);
         List<string>weaponType = new List<string>{ "knife", "gun", "M16", "AK47" };
         for (int i = 0; i < sum; i++)

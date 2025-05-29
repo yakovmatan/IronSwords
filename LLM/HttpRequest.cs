846caf02772bed8884531421411f26724e0f121c
yakov matan <yakovmatan@gmail.com>
2025-05-29 16:29:43 +0300

    create GetApiKey function

--- LLM/HttpRequest.cs
+++ LLM/HttpRequest.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DotNetEnv;
 
 namespace IronSwords.LLM
 {
-    internal class Class1
+    internal class HttpRequest
     {
+        public string GetKey()
+        {
+            Env.Load("C:\\Users\\User\\source\\repos\\IronSwords\\LLM\\.env");
+            string apiKey = Environment.GetEnvironmentVariable("GEMINI_API_KEY").ToString();
+            return apiKey;
+        }
     }
 }

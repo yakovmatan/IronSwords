3004b13c481cfb17961ec3e4c8cb3195229a4f9f
yakov matan <yakovmatan@gmail.com>
2025-05-30 12:45:36 +0300

    create GeminiResponse class

---
+++ HTTP/GeminiResponse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IronSwords.HTTP
+{
+    internal class GeminiResponse
+    {
+        public List<Candidate> candidates { get; set; }
+    }
+    public class Candidate
+    {
+        public Content  content { get; set; }
+    }
+    public class Content
+    {
+        public List<Part> Parts { get; set; }
+    }
+    public class Part
+    {
+        public string text { get; set; }
+    }
+}

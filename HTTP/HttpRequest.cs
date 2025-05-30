e0e622961ec6c4b63fcb33b88cfd1dd7c91795ee
yakov matan <yakovmatan@gmail.com>
2025-05-30 12:24:52 +0300

    create class http request

---
+++ HTTP/HttpRequest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Globalization;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net.Http;
+using System.Security.Policy;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using DotNetEnv;
+
+namespace IronSwords.LLM
+{
+    internal class HttpRequest
+    {
+        private static HttpClient Client = new HttpClient();
+        private string Apikey;
+        private string Url;
+
+        public HttpRequest()
+        {
+            Apikey = GetKey();
+        }
+        public string GetKey()
+        {
+            Env.Load("C:\\Users\\User\\source\\repos\\IronSwords\\LLM\\.env");
+            string apiKey = Environment.GetEnvironmentVariable("GEMINI_API_KEY").ToString();
+            return apiKey;
+        }
+
+        public async Task HttpPostToGemini(string requst)
+        {
+            this.Url = $"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key={this.Apikey}";
+            var body = new
+            {
+                contents = new[]
+                {
+                    new
+                    {
+                        parts = new[]
+                        {
+                            new {text = requst}
+                        }
+                    }
+                }
+            };
+            var json = JsonSerializer.Serialize(body)
+
+
+        }
+    }
+}
---
+++ HTTP/HttpRequester.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace IronSwords.HTTP
+{
+    internal class HttpRequester
+    {
+        private static readonly HttpClient Client = new HttpClient();
+        
+        public async Task<string> PostJsonAsync(string url,object body)
+        {
+            var json = JsonSerializer.Serialize(body);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await Client.PostAsync(url, content);
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
